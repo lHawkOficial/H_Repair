16adffc51fbf8103d06d5ccc932bb17ff55f3f45
lHawkOficial <luanvictorchagas2015@gmail.com>
2023-01-29 01:25:57 -0300

    updated

--- H_Repair/src/me/hrepair/commands/CommandRepair.java
+++ H_Repair/src/me/hrepair/commands/CommandRepair.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import me.hrepair.Core;
+import me.hrepair.objects.configs.ConfigGeral;
 import me.hrepair.objects.menus.MenuMain;
 import me.hrepair.utils.Mensagens;
 
@@ -21,7 +22,6 @@ public class CommandRepair implements CommandExecutor {
 		if (!(s instanceof Player)) return false;
 		Player p = (Player)s;
 		p.playSound(p.getLocation(), Sound.NOTE_BASS_GUITAR, 0.5f, 10);
-		//PlayerRepair pr = PlayerRepair.check(p);
 		if (args.length == 1) {
 			if (args[0].equalsIgnoreCase("reload")) {
 				if (p.hasPermission("H_Reparar.Adm")) {
@@ -31,8 +31,20 @@ public class CommandRepair implements CommandExecutor {
 				}
 			}
 		}
-		MenuMain.get().open(p);
-		return false;
+		if (p.hasPermission("H_Reparar.Command")) {
+			if (ConfigGeral.get().getWorlds().contains(p.getWorld()) || p.hasPermission("H_Reparar.Worlds")) {
+				MenuMain.get().open(p);
+				return false;
+			}else {
+				p.sendMessage(Mensagens.get().getCommandWorld());
+				p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1, 1);
+				return false;
+			}
+		} else {
+			p.sendMessage(Mensagens.get().getPermissionCommand());
+			p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1, 1);
+			return false;
+		}
 	}
 	
 }
